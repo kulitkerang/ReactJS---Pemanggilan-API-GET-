1777a110d955c7ad85a8354bdcc895de34dfe48a
Taufiqul Hidayah <bakarsapi@gmail.com>
2021-02-21 13:41:03 +0700

    First-Commit

---
+++ src/container/Blogpost/Blogpost.jsx
@@ -0,0 +1,34 @@
+import React, { Component, Fragment } from 'react';
+import Post from '../../component/Post/Post';
+import './Blogpost.css';
+
+class Blogpost extends Component {
+    state = {
+        post: []
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                post:json
+            })
+        })
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <p className="section-title">Blogpost</p>
+                {
+                    this.state.post.map(post => {
+                       return <Post key={post.id} title={post.title} desc={post.body} />
+                    })
+                }
+            </Fragment>
+        )
+    }
+}
+
+export default Blogpost;
